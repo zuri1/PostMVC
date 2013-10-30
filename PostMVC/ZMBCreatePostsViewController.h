f97c40f4a097e3c163f3c962da4bff09009df675
zuri1 <zuri.biringer@gmail.com>
2013-10-30 15:36:44 -0700

    Added ZMBCreatePostsViewController

--- PostMVC/ZMBCreatePostsViewController.h
+++ PostMVC/ZMBCreatePostsViewController.h
@@ -2,12 +2,15 @@
 //  ZMBCreatePostsViewController.h
 //  PostMVC
 //
-//  Created by Zuri Biringer on 10/25/13.
+//  Created by Zuri Biringer on 10/30/13.
 //  Copyright (c) 2013 Zuri Biringer. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "ZMBPost.h"
 
-@interface ZMBCreatePostsViewController : UITableViewController
+@interface ZMBCreatePostsViewController : UIViewController
+
+@property (nonatomic, weak) ZMBPost *post;
 
 @end
